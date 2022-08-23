49a9a41aa6c81314d24de69459d348da60feec96
Ting0887 <lutingyang@gmail.com>
2022-08-23 12:35:53 +0800

    add same email check

--- account/forms.py
+++ account/forms.py
@@ -4,6 +4,7 @@ from account.models import User
 
 class UserForm(forms.ModelForm):
     username = forms.CharField(label='帳號')
+    email = forms.CharField(label='電子信箱', widget=forms.EmailInput, max_length=128)
     password = forms.CharField(label='密碼', widget=forms.PasswordInput, )
     password2 = forms.CharField(label='確認密碼', widget=forms.PasswordInput)
     fullName = forms.CharField(label='姓名', max_length=128)
@@ -12,7 +13,7 @@ class UserForm(forms.ModelForm):
     
     class Meta:
         model = User
-        fields = ['username', 'password', 'password2', 'fullName', 'website', 'address']
+        fields = ['username', 'password', 'password2', 'fullName', 'website', 'address', 'email']
         
     def clean_password2(self):
         password = self.cleaned_data.get('password')
@@ -20,6 +21,13 @@ class UserForm(forms.ModelForm):
         if password and password2 and password != password2:
             raise forms.ValidationError('密碼不相符')
         return password2
+    
+    def clean_email(self):    
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email):
+            raise forms.ValidationError('此信箱已被註冊')
+        return email
+    
     def save(self):
         user = super().save(commit=False)
         user.set_password(user.password)
